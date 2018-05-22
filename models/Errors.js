e186b45403b0e8f917c6a6ea163768298b448dfc
Niek Flipse <npaflips@avans.nl>
2018-05-22 11:47:33 +0200

    We are now able to call the most common http errors

---
+++ models/Errors.js
@@ -0,0 +1,31 @@
+const Error = require('./Error');
+
+class Errors {
+    static unauthorized() {
+        return new Error("Not authorized", 401);
+    }
+
+    static noValidToken() {
+        return new Error("Token is invalid", 498);
+    }
+
+    static conflict() {
+        return new Error("Conflict", 409);
+    }
+
+    static badRequest() {
+        return new Error("Bad Request", 400);
+    }
+
+    static forbidden() {
+        return new Error("Forbidden", 403);
+    }
+
+    static resquesTimeout() {
+        return new Error("Request Timeout", 408);
+    }
+
+    static internalServerError() {
+        return new Error("Internal Server Error", 500);
+    }
+}
