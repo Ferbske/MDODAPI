20b57e69370ae1c4333eb6039ab5aa9884aba822
Niek Flipse <npaflips@avans.nl>
2018-05-22 12:24:17 +0200

    Added comments to Errors.js file and fixed a typo

--- models/Errors.js
+++ models/Errors.js
@@ -1,35 +1,75 @@
 const Error = require('./Error');
 
 class Errors {
+    /**
+     * Similar to 403 Forbidden,
+     * but specifically for use when authentication is required and has failed or has not yet been provided.
+     * The response must include a X-Access-Token header field containing a challenge applicable to the requested resource.
+     * @returns {Error} Not authorized 401.
+     */
     static unauthorized() {
         return new Error("Not authorized", 401);
     }
 
+    /**
+     * Method that will be used when a user sends an invalid token
+     * @returns {Error} Invalid Token 498.
+     */
     static noValidToken() {
-        return new Error("Token is invalid", 498);
+        return new Error("Invalid Token", 498);
     }
 
+    /**
+     * Indicates that the request could not be processed because of conflict in the request
+     * @returns {Error} Conflict 409.
+     */
     static conflict() {
         return new Error("Conflict", 409);
     }
 
+    /**
+     * The server cannot or will not process the request due to an apparent client error
+     * e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing).
+     * @returns {Error} Bad Request 400.
+     */
     static badRequest() {
         return new Error("Bad Request", 400);
     }
 
+    /**
+     * The request was valid, but the server is refusing action.
+     * The user might not have the necessary permissions for a resource, or may need an account of some sort.
+     * @returns {Error} Forbidden 403.
+     */
     static forbidden() {
         return new Error("Forbidden", 403);
     }
 
-    static resquesTimeout() {
+    /**
+     *
+     * @returns {Error}
+     */
+    static requestTimeout() {
         return new Error("Request Timeout", 408);
     }
 
+    /**
+     * A generic error message,
+     * given when an unexpected condition was encountered and no more specific message is suitable.
+     * @returns {Error}
+     */
     static internalServerError() {
         return new Error("Internal Server Error", 500);
     }
 
+    /**
+     * The requested resource could not be found but may be available in the future.
+     * Subsequent requests by the client are permissible.
+     * @returns {Error}
+     */
     static notFound() {
         return new Error("Not Found", 404);
     }
 }
+
+module.exports = Errors;
