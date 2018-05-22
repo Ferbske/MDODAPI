04426f1dff57d5c76920ac0c64d7d42de77f0336
Yannick Willems <ywillems96@gmail.com>
2018-05-22 17:12:25 +0200

    Updated user models

--- models/Client.js
+++ models/Client.js
@@ -1,14 +1,21 @@
 const User = require('./User')
 
 class Client extends User {
-    constructor(email, password, firstname, lastname, dob){
-        super(email, password, firstname, lastname)
+    constructor(email, password, firstname, lastname, phonenumber, dob, city, address, postal){
+        super(email, password, firstname, lastname, phonenumber)
         this.dob = dob
+        this.city = city
+        this.address = address
+        this.postal = postal
+        this.psych = null
     }
 
-    constructor(email, password, firstname, lastname, dob, psych){
-        super(email, password, firstname, lastname)
+    constructor(email, password, firstname, lastname, phonenumber, dob, city, address, postal, psych){
+        super(email, password, firstname, lastname, phonenumber)
         this.dob = dob
+        this.city = city
+        this.address = address
+        this.postal = postal
         this.psych = psych
     }
 
@@ -20,6 +27,30 @@ class Client extends User {
         this.dob = dob
     }
 
+    get city(){
+        return this.city
+    }
+
+    set city(city){
+        this.city = city
+    }
+
+    get address(){
+        return this.address
+    }
+
+    set address(address){
+        this.address = address
+    }
+
+    get postal(){
+        return this.postal
+    }
+
+    set postal(postal){
+        this.postal = postal
+    }
+
     get psych(){
         return this.psych
     }
@@ -28,3 +59,5 @@ class Client extends User {
         this.psych = psych
     }
 }
+
+module.exports = Client
