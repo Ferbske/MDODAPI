a3196af30013c035aebabac707ae3ef1d25315b4
Stijn van Veen <stijnboz@live.nl>
2018-05-23 16:13:59 +0200

    added more tests for registration

--- test/register_routes_test.js
+++ test/register_routes_test.js
@@ -67,7 +67,7 @@ describe('Registration', function () {
                 "password" : "wachtwoord"
             })
             .end((err, res) => {
-                res.should.have.status(403);
+                res.should.have.status(409);
                 res.body.should.be.a('object');
                 done();
             });
@@ -90,9 +90,225 @@ describe('Registration', function () {
                 "zipcode" : "6969 HB"
             })
             .end((err, res) => {
-                res.should.have.status(401);
+                res.should.have.status(409);
                 res.body.should.be.a('object');
                 done();
             });
     });
+
+    it('PSYCHOLOGIST: should throw an error when no firstname is provided', (done) => {
+        chai.request(index)
+            .post('/api/register/psychologist')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname" : "",
+                "infix" : "van",
+                "lastname" : "Veen",
+                "phonenumber": "0629456850",
+                "location" : "Bergen op Zoom",
+                "email" : "stijnboz@live.nl",
+                "password" : "wachtwoord"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('CLIENT: should throw an error when no firstname is provided', (done) => {
+        chai.request(index)
+            .post('/api/register/client')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname": "",
+                "infix": "",
+                "lastname": "Neus",
+                "dateofbirth": "1996-11-27",
+                "email": "sjaak@gmail.com",
+                "password": "qwerty123",
+                "phonenumber": "062345678",
+                "city" : "Breda",
+                "adress" : "Zuidsingel 8",
+                "zipcode" : "6969 HB"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('PSYCHOLOGIST: should throw an error when firstname is shorter than 2 chars', (done) => {
+        chai.request(index)
+            .post('/api/register/psychologist')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname" : "s",
+                "infix" : "van",
+                "lastname" : "Veen",
+                "phonenumber": "0629456850",
+                "location" : "Bergen op Zoom",
+                "email" : "stijnboz@live.nl",
+                "password" : "wachtwoord"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('CLIENT: should throw an error when firstname is shorter than 2 chars', (done) => {
+        chai.request(index)
+            .post('/api/register/client')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname": "k",
+                "infix": "",
+                "lastname": "Neus",
+                "dateofbirth": "1996-11-27",
+                "email": "sjaak@gmail.com",
+                "password": "qwerty123",
+                "phonenumber": "062345678",
+                "city" : "Breda",
+                "adress" : "Zuidsingel 8",
+                "zipcode" : "6969 HB"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('PSYCHOLOGIST: should throw an error when no lastname is provided', (done) => {
+        chai.request(index)
+            .post('/api/register/psychologist')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname" : "stijn",
+                "infix" : "van",
+                "lastname" : "",
+                "phonenumber": "0629456850",
+                "location" : "Bergen op Zoom",
+                "email" : "stijnboz@live.nl",
+                "password" : "wachtwoord"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('CLIENT: should throw an error when no lastname is provided', (done) => {
+        chai.request(index)
+            .post('/api/register/client')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname": "sjaak",
+                "infix": "",
+                "lastname": "",
+                "dateofbirth": "1996-11-27",
+                "email": "sjaak@gmail.com",
+                "password": "qwerty123",
+                "phonenumber": "062345678",
+                "city" : "Breda",
+                "adress" : "Zuidsingel 8",
+                "zipcode" : "6969 HB"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('PSYCHOLOGIST: should throw an error when lasstname is shorter than 2 chars', (done) => {
+        chai.request(index)
+            .post('/api/register/psychologist')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname" : "Stijn",
+                "infix" : "van",
+                "lastname" : "V",
+                "phonenumber": "0629456850",
+                "location" : "Bergen op Zoom",
+                "email" : "stijnboz@live.nl",
+                "password" : "wachtwoord"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('CLIENT: should throw an error when lastname is shorter than 2 chars', (done) => {
+        chai.request(index)
+            .post('/api/register/client')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname": "Sjaak",
+                "infix": "",
+                "lastname": "N",
+                "dateofbirth": "1996-11-27",
+                "email": "sjaak@gmail.com",
+                "password": "qwerty123",
+                "phonenumber": "062345678",
+                "city" : "Breda",
+                "adress" : "Zuidsingel 8",
+                "zipcode" : "6969 HB"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('PSYCHOLOGIST: should throw an error when email is invalid', (done) => {
+        chai.request(index)
+            .post('/api/register/psychologist')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname" : "Stijn",
+                "infix" : "van",
+                "lastname" : "Veen",
+                "phonenumber": "0629456850",
+                "location" : "Bergen op Zoom",
+                "email" : "stijnboz@live",
+                "password" : "wachtwoord"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
+    it('CLIENT: should throw an error when email is invalid', (done) => {
+        chai.request(index)
+            .post('/api/register/client')
+            .set('Content-Type', 'application/json')
+            .send({
+                "firstname": "Sjaak",
+                "infix": "",
+                "lastname": "N",
+                "dateofbirth": "1996-11-27",
+                "email": "@gmail.com",
+                "password": "qwerty123",
+                "phonenumber": "062345678",
+                "city" : "Breda",
+                "adress" : "Zuidsingel 8",
+                "zipcode" : "6969 HB"
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                done();
+            });
+    });
+
 });
