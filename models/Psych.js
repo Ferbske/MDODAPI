d23bdb69af6bf12a64d8914e00f2a1584e996d3b
Niek Flipse <npaflips@avans.nl>
2018-05-23 14:38:33 +0200

    Psych is no longer extending from User

--- models/Psych.js
+++ models/Psych.js
@@ -1,40 +1,28 @@
-const User = require('./User')
 const Errors = require('./Errors');
 
-class Psych extends User{
-    constructor(email, password, firstname, lastname, location){
-        if(!(
+class Psychologist{
+    constructor(email, password, firstname, infix, lastname, location, phonenumber){
+        if (!(
+            email && /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]/.test(email) &&
+            password && /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,80}$/.test(password) &&
+            firstname && /^[A-Za-z]{2,50}$/.test(firstname) &&
+            (infix === "" || infix && /^[A-Za-z]{2,8}(\s[A-Z-a-z]{2,8})*/.test(infix)) &&
+            lastname && /^[A-Za-z]{2,50}$/.test(lastname) &&
+            phonenumber &&
             location && /^[A-Za-z]{2,25}/.test(location)
         )) {
-            return new Errors.badRequest();
+            console.log("Before Errors.badRequest");
+            return Errors.badRequest();
         }
 
-        super(email, password, firstname, lastname)
-        this.location = location
-        this.clients = []
-    }
-
-    constructor(email, password, firstname, lastname, location, clients){
-        super(email, password, firstname, lastname)
-        this.location = location
-        this.clients = clients
-    }
-
-    get location(){
-        return this.location
-    }
-
-    set location(location){
-        this.location = location
-    }
-
-    get clients(){
-        return this.clients
-    }
-
-    set clients(clients){
-        this.clients = clients
+        this._email = email;
+        this._password = password;
+        this._firstname = firstname;
+        this._lastname = lastname;
+        this._phonenumber = phonenumber;
+        this._location = location;
+        this._clients = []
     }
 }
 
-module.exports = Psych
+module.exports = Psychologist;
