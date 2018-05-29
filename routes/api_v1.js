5fd6a7418fe66575b1634528d0df4d68634dc9da
Niek Flipse <npaflips@avans.nl>
2018-05-29 13:31:39 +0200

    Created GET POST and DELETE request for Goals

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -6,43 +6,42 @@ const Errors = require('../models/Errors');
 const db = require('../db/databaseConnector');
 const Goal = require('../models/Goal');
 
-/**
- * Decode the token and return the payload.
- * @param res The response to print json if the decodeToken function fails
- * @param cb The callback function that will be called if the payload is successfully retrieved.
- */
-function authDecodeToken(req, res, cb) {
-    const token = req.header('X-Access-Token');
-    auth.decodeToken(token, (error, payload) => {
-        if (error) {
-            console.log(error);
-            const err = Errors.unauthorized();
-            res.status(err.code).json(err);
-            return;
-        }
-
-        cb(null, payload);
-    })
-}
-
-router.route('/goals')
+router.route('/goals/:goalId?')
     .get((req, res) => {
-        authDecodeToken(req, res, (error, payload) => {
+        const token = req.header('X-Access-Token');
+        auth.decodeToken(token, (error, payload) => {
+            if (error) {
+                console.log(error);
+                const err = Errors.noValidToken();
+                res.status(err.code).json(err);
+                return;
+            }
+
             const email = payload.sub;
-            db.query("SELECT description FROM mdod.Goals WHERE email = ?", [email], (error, rows, fields) => {
+            db.query("SELECT goalId, description FROM mdod.Goals WHERE email = ?", [email], (error, rows, fields) => {
+                if (error) {
+                    const err = Errors.conflict();
+                    res.status(err.code).json(err);
+                    return;
+                }
                 res.status(200).json(rows);
-            })
-        })
+            });
+        });
     })
     .post((req, res) => {
-        authDecodeToken(req, res, (error, payload) => {
+        const token = req.header('X-Access-Token') || '';
+        auth.decodeToken(token, (error, payload) => {
+            if (error) {
+                console.log(error);
+                const err = Errors.noValidToken();
+                res.status(err.code).json(err);
+            }
             const email = payload.sub;
             const description = req.body.description || '';
 
             const goal = new Goal(description);
 
             if (goal._description) {
-                // TODO: Check if goal exists.
                 db.query("INSERT INTO mdod.Goals(email, description) VALUES(?, ?)", [email, goal._description], (error, result) => {
                     if (error) {
                         console.log(error);
@@ -52,14 +51,54 @@ router.route('/goals')
                     }
 
                     res.status(201).json({
-                        "message": "Goal aangemaakt"
+                        message: "Goal aangemaakt"
                     })
                 })
             } else {
                 res.status(goal.code).json(goal);
             }
-        })
+        });
     });
 
+router.delete("/goals/:goalId", (req, res) => {
+    // Get the token from the request
+    const token = req.header('X-Access-Token') || '';
+
+    // Decode the token.
+    auth.decodeToken(token, (error, payload) => {
+        // If token is not valid. Return noValidToken error to the user
+        if (error) {
+            console.log(error);
+            const err = Errors.noValidToken();
+            res.status(err.code).json(err);
+            return;
+        }
+
+        // Get the goalId from the request. The goal with this id wil be deleted
+        const goalId = req.params.goalId;
+
+        // Get the email of the person who would like to delete the goal.
+        const email = payload.sub;
+        db.query("DELETE FROM mdod.Goals WHERE goalId = ? AND email = ?", [goalId, email], (error, result) => {
+            if (error) {
+                console.log(error);
+                const err = Errors.conflict();
+                res.status(err.code).json(err);
+                return;
+            }
+
+            if (result.affectedRows < 1) {
+                console.log("0 rows affected");
+                const error = Errors.forbidden();
+                res.status(error.code).json(error);
+                return;
+            }
+
+            res.status(200).json({
+                message: "Goal verwijderd."
+            })
+        })
+    })
+});
 
 module.exports = router;
