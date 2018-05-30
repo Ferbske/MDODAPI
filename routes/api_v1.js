fb26beca7446c06ab837c4e007b6eaff82c64267
Stijn van Veen <stijnboz@live.nl>
2018-05-30 14:02:22 +0200

    added /pickclient endpoint to add a psychologist to a client

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -292,7 +292,7 @@ router.get('/all/:role', (req, res) => {
             let error = Errors.noValidToken();
             res.status(error.code).json(error);
         } else {
-            if (role === 'client'){
+            if (role === 'client') {
                 const email = payload.sub;
                 db.query("SELECT email FROM mdod.Psychologist WHERE email = ?;", [email], (error, rows) => {
                     if (error) {
@@ -306,24 +306,24 @@ router.get('/all/:role', (req, res) => {
                         return;
                     }
                     if (rows.length > 0) {
-                       db.query("SELECT email, firstname, infix, lastname FROM mdod.Client",[email], (error, rows) => {
-                           if (error) {
-                               const err = Errors.unknownError();
-                               res.status(err.code).json(err);
-                               return;
-                           }
-                           if (rows.length < 1) {
-                               let error = Errors.notFound();
-                               res.status(error.code).json(error);
-                               return;
-                           }
-                           if(rows.length > 0) {
-                               res.status(200).json(rows);
-                           }
-                       });
+                        db.query("SELECT email, firstname, infix, lastname FROM mdod.Client", [email], (error, rows) => {
+                            if (error) {
+                                const err = Errors.unknownError();
+                                res.status(err.code).json(err);
+                                return;
+                            }
+                            if (rows.length < 1) {
+                                let error = Errors.notFound();
+                                res.status(error.code).json(error);
+                                return;
+                            }
+                            if (rows.length > 0) {
+                                res.status(200).json(rows);
+                            }
+                        });
                     }
                 });
-            }else {
+            } else {
                 const err = Errors.badRequest();
                 res.status(err.code).json(err);
             }
@@ -340,7 +340,7 @@ router.post('/specific/:role', (req, res) => {
             let error = Errors.noValidToken();
             res.status(error.code).json(error);
         } else {
-            if (role === 'client'){
+            if (role === 'client') {
                 const email = payload.sub;
                 const client_email = req.body.email || "";
                 db.query("SELECT email FROM mdod.Psychologist WHERE email = ?;", [email], (error, rows) => {
@@ -355,7 +355,7 @@ router.post('/specific/:role', (req, res) => {
                         return;
                     }
                     if (rows.length > 0) {
-                        db.query("SELECT email, contact, phonenumber, birthday, city, adress, zipcode, firstname, infix, lastname FROM mdod.Client WHERE email = ?;",[client_email], (error, rows) => {
+                        db.query("SELECT email, contact, phonenumber, birthday, city, adress, zipcode, firstname, infix, lastname FROM mdod.Client WHERE email = ?;", [client_email], (error, rows) => {
                             if (error) {
                                 const err = Errors.unknownError();
                                 res.status(err.code).json(err);
@@ -366,16 +366,52 @@ router.post('/specific/:role', (req, res) => {
                                 res.status(error.code).json(error);
                                 return;
                             }
-                            if(rows.length > 0) {
+                            if (rows.length > 0) {
                                 res.status(200).json(rows);
                             }
                         });
                     }
                 });
-            }else {
+            } else {
                 const err = Errors.badRequest();
                 res.status(err.code).json(err);
             }
+        }
+    });
+});
+
+router.put('/pickclient', (req, res) => {
+    const token = (req.header('X-Access-Token')) || '';
+    const data = auth.decodeToken(token, (err, payload) => {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            let error = Errors.noValidToken();
+            res.status(error.code).json(error);
+        } else {
+            const email = payload.sub;
+            const client_email = req.body.email || "";
+            db.query("SELECT email FROM mdod.Psychologist WHERE email = ?;", [email], (error, rows) => {
+                if (error) {
+                    console.log(error);
+                    const err = Errors.unknownError();
+                    res.status(err.code).json(err);
+                    return;
+                }
+                if (rows.length < 1) {
+                    let error = Errors.notFound();
+                    res.status(error.code).json(error);
+                }
+                else if (rows.length > 0) {
+                    db.query("UPDATE mdod.Client SET contact = ? WHERE email = ?", [email, client_email], (error, result) => {
+                        if (error) {
+                            console.log(error);
+                            const err = Errors.unknownError();
+                            res.status(err.code).json(err);
+                        }
+                        res.status(202).json({message: "Client Aangepast"});
+                    });
+                }
+            });
         }
     });
 });
