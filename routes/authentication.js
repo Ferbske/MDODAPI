35f8490cafdbf2ad2e2e064b7e3a3e4b4e90b143
Stijn van Veen <stijnboz@live.nl>
2018-05-31 14:16:19 +0200

    changed the tests, and fixed some typos

--- routes/authentication.js
+++ routes/authentication.js
@@ -358,7 +358,7 @@ router.delete("/:role", (req, res) => {
                         res.status(err.code).json(err);
                         return;
                     }
-                    res.status(202).json({message: "Client Verwijderd"})
+                    res.status(202).json({message: "Cliënt Verwijderd"})
                 });
             }
             else if (role === 'psychologist') {
@@ -368,7 +368,7 @@ router.delete("/:role", (req, res) => {
                         res.status(err.code).json(err);
                         return;
                     }
-                    res.status(200).json({message: "Psychologist Verwijderd"});
+                    res.status(200).json({message: "Psycholoog Verwijderd"});
                 });
             } else {
                 const err = Errors.badRequest();
