96a8a22e08cd74b44d677bad8a6a2a7100aa2e10
Niek Flipse <npaflips@avans.nl>
2018-06-05 13:38:17 +0200

    Added error handling to /all route on substances

--- routes/substance.js
+++ routes/substance.js
@@ -5,6 +5,15 @@ const db = require('../db/databaseConnector');
 const auth = require('../auth/authentication');
 const Errors = require('../models/Errors');
 
+router.route('/')
+    .get((req, res) => {
+        const token = global.stripBearerToken(req.header('Authorization'));
+
+        auth.decodeToken(token, (error, payload) => {
+
+        })
+    });
+
 router.route('/all')
     .get((req, res) => {
         const token = global.stripBearerToken(req.header('Authorization'));
@@ -17,6 +26,17 @@ router.route('/all')
             }
 
             db.query("SELECT * FROM mdod.Substance", (error, rows, fields) => {
+                if (error) {
+                    console.log(error);
+                    const err = Errors.conflict();
+                    res.status(err.code).json(err);
+                }
+
+                if (rows.length < 1) {
+                    const error = Errors.notFound();
+                    res.status(error.code).json(error);
+                }
+                
                 res.status(200).json(rows);
             })
         })
