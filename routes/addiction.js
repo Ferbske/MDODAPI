c6deb1cc4e36f182ccaf6b145fdc88958027f7e4
Niek Flipse <npaflips@avans.nl>
2018-06-05 14:02:43 +0200

    Created route to get all the addictions for a single client

--- routes/addiction.js
+++ routes/addiction.js
@@ -2,8 +2,34 @@ const express = require('express');
 const router = express.Router({});
 
 router.route('/')
-    .post((req, res) => {
+/**
+ * Get all the addictions for a single client.
+ */
+    .get((req, res) => {
+        const token = global.stripBearerToken(req.header('Authorization'));
 
+        auth.decodeToken(token, (error, payload) => {
+            const email = payload.sub;
+
+            db.query("SELECT mdod.Addiction.id, mdod.Addiction.substanceId, mdod.Addiction.email, mdod.Substance.`type`, mdod.Substance.name, mdod.Substance.measuringUnit\n" +
+                "FROM mdod.Addiction \n" +
+                "\tINNER JOIN mdod.Substance ON mdod.Addiction.substanceId = mdod.Substance.id\n" +
+                "\tWHERE mdod.Addiction.email = '?';", [email], (error, rows, fields) => {
+                if (error) {
+                    console.log(error);
+                    const err = Errors.conflict();
+                    res.status(err.code).json(err);
+                    return;
+                }
+
+                if (rows.length < 1) {
+                    const error = Errors.notFound();
+                    res.status(error.code).json(error);
+                    return;
+                }
+
+                res.status(200).json(rows);
+            })
+        })
     });
-
 module.exports = router;
