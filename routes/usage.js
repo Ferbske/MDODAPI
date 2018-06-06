168bf457ceab14832fcf2a1d0da079c357d94988
Yannick Willems <ywillems96@gmail.com>
2018-06-06 14:32:59 +0200

    Fixed psychologist endpoints

--- routes/usage.js
+++ routes/usage.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router({});
+const usageData = require('./usageData');
 const auth = require('../auth/authentication');
 const Errors = require('../models/Errors');
 const db = require('../db/databaseConnector');
 const Usage = require('../models/Usage');
 const global = require('../globalFunctions');
+
+//Psychologist routes
+router.use('/client/data', usageData);
 
 //CRUD Actions
 router.route('/:usageId?')
@@ -198,70 +202,5 @@ router.get('/clean/status', (req, res) => {
             });
     });
 });
-
-//Psychologist endpoints
-router.put('/', (req, res) => {
-    const token = global.stripBearerToken(req.header('Authorization'));
-
-    auth.decodeToken(token, (error, payload) => {
-        if (error) {
-            console.log(error);
-            const err = Errors.noValidToken();
-            res.status(err.code).json(err);
-        }
-        const psychEmail = payload.sub;
-        const clientEmail = req.body.clientEmail || '';
-
-        if (clientEmail) {
-            db.query("SELECT mdod.Usage.email, mdod.`Usage`.substanceId, mdod.`Usage`.description, " +
-                "mdod.`Usage`.usedAt FROM mdod.`Usage` WHERE email = ?", [clientEmail], (error, rows, fields) => {
-
-                    if (error) {
-                        console.log(error);
-                        const err = Errors.conflict();
-                        res.status(err.code).json(err);
-                        return;
-                    }
-
-                    res.status(200).json(rows)
-                })
-        } else {
-            res.status(usage.code).json(usage);
-        }
-    });
-})
-
-router.put('/clean/status', (req, res) => {
-    const token = global.stripBearerToken(req.header('Authorization'));
-
-    auth.decodeToken(token, (error, payload) => {
-        if (error) {
-            console.log(error);
-            const err = Errors.noValidToken();
-            res.status(err.code).json(err);
-            return;
-        }
-
-        const psychEmail = payload.sub
-        const clientEmail = req.body.clientEmail || '';
-
-        db.query("SELECT DATEDIFF(CURDATE(), MAX(mdod.`Usage`.usedAt)) AS daysClean " +
-            "FROM mdod.Usage " +
-            "INNER JOIN mdod.Substance ON mdod.Usage.substanceId = mdod.Substance.id " +
-            "WHERE mdod.Usage.email = ?;", [clientEmail], (error, rows, fields) => {
-                if (error) {
-                    const err = Errors.conflict();
-                    res.status(200).json(err);
-                    return;
-                } else {
-                    const daysClean = rows[0].daysClean
-
-                    res.status(200).json({
-                        "daysClean": daysClean
-                    })
-                }
-            });
-    });
-})
 
 module.exports = router;
