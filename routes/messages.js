a067fd87d7cda80f80fcd9b437564dcd3f954b16
Stijn van Veen <stijnboz@live.nl>
2018-06-12 12:00:27 +0200

    Added post endpoint for a message as a spychologist

--- routes/messages.js
+++ routes/messages.js
@@ -32,12 +32,12 @@ router.post('/client', (req, res) => {
                             if (error) {
                                 const err = Errors.conflict();
                                 res.status(err.code).json(err);
-                            }else{
+                            } else {
                                 res.status(200).json({message: "bericht verstuurd"});
                             }
                         });
                     } else {
-                        res.status(400).json({"message": "Berichten niet mogelijk, u bent niet de psycholoog, of de cliënt heeft nog geen psycholoog gekoppelt."})
+                        res.status(400).json({message: "Berichten niet mogelijk, u bent niet de psycholoog, of de cliënt heeft nog geen psycholoog gekoppelt."})
                     }
                 }
             });
@@ -64,8 +64,8 @@ router.get('/client', (req, res) => {
                     let error = Errors.notFound();
                     res.status(error.code).json(error);
                 }
-                else if (rows.length > 0){
-                    db.query("SELECT email_client, email_psych, message, date FROM mdod.Messages WHERE email_client = ? ORDER BY date DESC",[email], (error, rows) => {
+                else if (rows.length > 0) {
+                    db.query("SELECT email_psych, message, date FROM mdod.Messages WHERE email_client = ? ORDER BY date DESC", [email], (error, rows) => {
                         if (error) {
                             const err = Errors.conflict();
                             res.status(err.code).json(err);
@@ -78,6 +78,58 @@ router.get('/client', (req, res) => {
         }
     });
 });
+
+router.post('/psychologist', (req, res) => {
+    const token = global.stripBearerToken(req.header('Authorization'));
+    const data = auth.decodeToken(token, (err, payload) => {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            let error = Errors.noValidToken();
+            res.status(error.code).json(error);
+        } else {
+            const email = payload.sub;
+            db.query("SELECT email FROM mdod.Psychologist WHERE email = ?;", [email], (error, rows, result) => {
+                if (error) {
+                    console.log(error);
+                    const err = Errors.unknownError();
+                    res.status(err.code).json(err);
+                    return;
+                }
+                if (rows.length < 1) {
+                    let error = Errors.notFound();
+                    res.status(error.code).json(error);
+                } else if (rows.length > 0) {
+                    const clientEmail = req.body.email;
+                    db.query("SELECT email FROM mdod.Client WHERE email = ?;", [clientEmail], (error, rows) => {
+                        if (error) {
+                            console.log(error);
+                            const err = Errors.unknownError();
+                            res.status(err.code).json(err);
+                            return;
+                        }
+                        if (rows.length < 1) {
+                            let error = Errors.notFound();
+                            res.status(error.code).json(error);
+                        } else if (rows.length > 0) {
+                            const message = req.body.message;
+                            db.query("INSERT INTO mdod.Messages (email_client, email_psych, message) VALUES (?, ?, ?);", [clientEmail, email, message], (err, result) => {
+                                if (error) {
+                                    const err = Errors.conflict();
+                                    res.status(err.code).json(err);
+                                } else {
+                                    res.status(200).json({message: "bericht verstuurd"});
+                                }
+                            });
+                        } else {
+                            res.status(400).json({message: "Berichten niet mogelijk, u bent niet de psycholoog, of de cliënt heeft nog geen psycholoog gekoppelt."})
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
 
 
 module.exports = router;
