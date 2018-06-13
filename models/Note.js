b2953917212c6dbb6ca07136ad9f26318081f7e9
Niek Flipse <npaflips@avans.nl>
2018-06-13 18:22:13 +0200

    Fixed regex

--- models/Note.js
+++ models/Note.js
@@ -3,12 +3,13 @@ const Errors = require('./Errors');
 class Note {
     constructor(title, description) {
         if (!(
-            (title === '' || title && /^[^()+=@#$%^&*~]$/.test(title))
-            (description === '' || description && /^[^()@#%^&*+=~]$/.test(description))
+            (title === '' || title && /^[^()+=@#$%^&*~]{0,50}$/.test(title)) &&
+            (description === '' || description && /^[^()@#%^&*+=~]{0,255}$/.test(description))
         )) {
             return Errors.badRequest();
         }
 
+        this._title = title;
         this._description = description;
     };
 }
