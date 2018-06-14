2523d39eb06bbe7b5d7586746da333f323cef3be
Niek Flipse <npaflips@avans.nl>
2018-06-14 13:38:06 +0200

    Changed comment

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router({});
 const authentication = require('./authentication');
 const v1 = require('./api_v1');
 
-//Routers for authentication and V1
+// Routers for authentication and V1.
 router.use("/", authentication);
 router.use("/v1", v1);
 
