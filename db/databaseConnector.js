7be45f153e6c7df636815478bfa5b4af735a5ade
Niek Flipse <npaflips@avans.nl>
2018-06-14 22:31:35 +0200

    Added doc comments to databaseConnection file

--- db/databaseConnector.js
+++ db/databaseConnector.js
@@ -1,7 +1,10 @@
 const mysql = require('mysql');
 const config = require('../config');
 
-// When the variables in precess.env are set (config vars in Heroku), user them instead of config.json
+/**
+ * When the variables in precess.env are set (config vars in Heroku), user them instead of config.json.
+ * @type {{host: string, username: string, password: string, name: string}}
+ */
 const dbConfig = process.env.DB_DATABASE && process.env.DB_USER && process.env.DB_PASSWORD && process.env.DB_HOST ? {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
@@ -9,6 +12,10 @@ const dbConfig = process.env.DB_DATABASE && process.env.DB_USER && process.env.D
     name: process.env.DB_DATABASE
 } : config.database;
 
+/**
+ * Create the database connection and store it in this variable.
+ * @type {Connection}
+ */
 const db = mysql.createConnection({
     host: dbConfig.host,
     user: dbConfig.username,
